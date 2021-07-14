e5aefdef39598217ccd098bc5fc60fd82d78eb24
caballo <imanol.l.p@hotmail.com>
2021-07-14 12:40:02 +0200

    Finished carousel

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Simple carousel created as a part of The Odin Project curriculum.
+Carousel created as a part of The Odin Project curriculum.
+Instructions: Simply add as many images as you want to the .slide-container div.
+The carousel will fit its parent height or width. 
+You can add as many carousels as you like in the same page without repeating javascript code.
+Simply copy the whole html code and add some more images to the new one.
---
+++ carousel.js
@@ -0,0 +1,61 @@
+const getAll = (clss) => document.querySelectorAll(clss);
+
+const arrowsLeft = getAll('.arrow-left');
+const arrowsRight = getAll('.arrow-right');
+const imageFrames = getAll('.slide-container');
+const dotContainers = getAll('.dot-container');
+
+for (let i = 0; i < imageFrames.length; i++) {
+	const arrowLeft = arrowsLeft[i];
+	const arrowRight = arrowsRight[i];
+	const imageFrame = imageFrames[i];
+	const dotContainer = dotContainers[i];
+	const images = imageFrame.children;
+
+	for (let j = 0; j < images.length; j++) {
+		const dot = document.createElement('div');
+		dot.classList.add('dot');
+		dot.setAttribute('data-index', `${j}`);
+		dotContainer.appendChild(dot);
+
+		dot.addEventListener('click', (e) => dotNavigation(e.target.dataset.index));
+	}
+
+    const dots = dotContainer.children;
+	let count = 0;
+
+	const next = () => {
+		count++;
+		if (count >= images.length) count = 0;
+		imageFrame.style.transform = `translateX(-${count}00%)`;
+        dotNavigation(count);
+	};
+
+	const prev = () => {
+		count--;
+		if (count < 0) count = images.length - 1;
+		imageFrame.style.transform = `translateX(-${count}00%)`;
+        dotNavigation(count);
+	};
+
+	const dotNavigation = (index) => {
+		imageFrame.style.transform = `translateX(-${index}00%)`;
+		count = index;
+		for (let y = 0; y < dots.length; y++) {
+			const dot = dots[y];
+			dot.classList.remove('dot-selected');
+			dots[index].classList.add('dot-selected');
+		}
+	};
+
+	const autoSlide = () => {
+		setInterval(() => {
+			next();
+		}, 5000);
+	};
+
+	arrowRight.addEventListener('click', () => next());
+	arrowLeft.addEventListener('click', () => prev());
+
+	autoSlide();
+}
